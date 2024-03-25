b31699eff61e5ffaaf8c6413c448144ab10de025
Aashritha Danthala <124041745+aashritha987@users.noreply.github.com>
2024-03-25 16:20:26 +0530

    Update firebase-config.js

--- netflix-clone/netflix-ui/src/utils/firebase-config.js
+++ netflix-clone/netflix-ui/src/utils/firebase-config.js
@@ -2,13 +2,7 @@ import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBCc1dvbQrzZ4eALlpRIxpJunESpx44XZU",
-  authDomain: "netflix-clone-b1b4a.firebaseapp.com",
-  projectId: "netflix-clone-b1b4a",
-  storageBucket: "netflix-clone-b1b4a.appspot.com",
-  messagingSenderId: "973755856102",
-  appId: "1:973755856102:web:a8da2eac414750c5d7f727",
-  measurementId: "G-VLSEZ3JCPH",
+  //should include your firebase api keys
 };
 
 const app = initializeApp(firebaseConfig);
